8cc0f51236600914afb7076122d8ab7dc6c3f95e
roylaurie <roy.laurie@gmail.com>
2020-04-13 10:38:31 -0700

    qr labels

---
+++ generate-qrlabels.mjs
@@ -0,0 +1,35 @@
+/**
+ * Generates a PDF file of QR Codes, each containing a randomly generated UUID.
+ **/
+'use strict';
+
+import QRCode from 'qrcode';
+import PDFDocument from 'pdfkit';
+import SVGtoPDF from 'svg-to-pdfkit';
+import uuid from 'uuidjs';
+import fs from 'fs';
+
+(()=>{
+    const QRCODE_SHEET_FILEPATH = './qrcode-sheet-pdf';
+    const pdfdoc = new PDFDocument();
+    let promises = [];
+
+    for (let y = pdfdoc.page.margins.top; y < (pdfdoc.page.height - pdfdoc.page.margins.bottom); y+=72) {
+            for (let x = pdfdoc.page.margins.left; x < (pdfdoc.page.width - pdfdoc.page.margins.right); x+=72) {
+                promises.push(addSVG(pdfdoc, x, y));
+            }
+    }
+    
+    Promise.all(promises).then(()=> {
+        pdfdoc.pipe(fs.createWriteStream(QRCODE_SHEET_FILEPATH));
+        pdfdoc.end();
+    });
+
+})();
+
+function addSVG(pdfdoc, x, y) {
+    let promise = QRCode.toString(uuid.genV4().hexNoDelim, { type: 'svg', width: 72, margin: 0 });
+    return promise.then((svg) => {
+        SVGtoPDF(pdfdoc, svg, x, y);
+    });
+};
