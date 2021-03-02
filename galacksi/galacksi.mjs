62302d2554d24d9c78df2dd5d36d73f158c0a390
roylaurie <roy.laurie@gmail.com>
2021-03-02 11:57:31 -0800

    big move

--- galacksi/galacksi.mjs
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-import Galacksi from './js/asmov/galacksi/Galacksi.mjs';
-
-(()=>{ Galacksi.start(); })();
--- galacksi/js/asmov/galacksi/duel/core/API.mjs
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import Meta from '../../plur/meta/Meta.mjs';
-import ModelMeta from '../../plur/meta/ModelMeta.mjs';
-
-export default {
-    namespace: 'asmov/galacksi/';
-    version: '2021.0.0';
-};
