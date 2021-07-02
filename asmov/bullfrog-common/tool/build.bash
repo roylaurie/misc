28695822e652d5fedff7210c3e0f6699a4bcf6ef
roylaurie <roy.laurie@gmail.com>
2021-07-02 14:41:07 -0700

    wip modules

--- asmov/bullfrog-common/tool/build.bash
+++ asmov/bullfrog-common/tool/build.bash
@@ -7,19 +7,22 @@ echo
 rm -rf ./dist
 
 # compact the namespace configuration schema
-cat bash/json/namespace.schema.json | jq -c > bash/json/namespace.schema.min.json
+cat json/schema/namespace.schema.json | jq -c > json/schema/namespace.schema.min.json
 
 # compact the actual namespace configuration into namespace.min.json
-cat bash/namespace.json | jq -c > bash/namespace.min.json 
+cat json/namespace.json | jq -c > json/namespace.min.json 
 
 # validate the minified namespace configuration against the minified schema
-ajv -s bash/json/namespace.schema.min.json -d bash/namespace.min.json 
+ajv -s json/schema/namespace.schema.min.json -d json/namespace.min.json 
 
 mkdir ./dist
 cp -r ./bash ./dist
+cp -r ./config ./dist
+cp -r ./json ./dist
+cp -r ./skeleton ./dist
 
-rm ./dist/bash/namespace.json
-rm ./dist/bash/json/namespace.schema.json
+rm ./dist/json/namespace.json
+rm ./dist/json/schema/namespace.schema.json
 
 echo
 echo "build successful"
