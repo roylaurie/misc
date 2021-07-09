ae1b7ebfbdab30db50de373f2e09663c372a69d3
roylaurie <roy.laurie@gmail.com>
2021-07-09 14:41:43 -0700

    frogusr basic init of usr config and usr data dirs

--- asmov/bullfrog-common/bash/lib/frogusr.lib.bash
+++ asmov/bullfrog-common/bash/lib/frogusr.lib.bash
@@ -5,7 +5,7 @@ frogusr_home () {
     realpath "$HOME"
 }
 
-_frogusr_dir () {
+_frogusr_config_dir () {
     local _app _config
     _app="$(frog_option_app)"
     _config="$(frog_option_config)"
@@ -21,9 +21,9 @@ _frogusr_data_dir () {
     echo "$HOME/.data/bullfrog/$_app/$_config"
 }
 
-frogusr_path () {
-    [[ -z "$_FROGUSR_PATH" ]] && frog_error 1 "Accessing frogusr_path before frogusr_init" "frogusr_path"
-    echo "$_FROGUSR_PATH"
+frogusr_config_path () {
+    [[ -z "$_FROGUSR_CONFIG_PATH" ]] && frog_error 1 "Accessing frogusr_path before frogusr_init" "frogusr_path"
+    echo "$_FROGUSR_CONFIG_PATH"
 }
 
 frogusr_data_path () {
@@ -32,18 +32,18 @@ frogusr_data_path () {
 }
 
 frogusr_init () {
-    local _app _config _usrdir _datadir
+    local _app _config _configdir _datadir
     _app="$(frog_option_app)"
     _config="$(frog_option_config)"
-    _usrdir="$(_frogusr_dir)"
+    _configdir="$(_frogusr_config_dir)"
     _datadir="$(_frogusr_data_dir)"
 
-    [[ -d "$_usrdir" ]] || mkdir -p "$_usrdir"
+    [[ -d "$_configdir" ]] || mkdir -p "$_configdir"
     [[ -d "$_datadir" ]] || mkdir -p "$_datadir"
 
-    _FROGUSR_PATH="$(realpath "$_usrdir")"
+    _FROGUSR_CONFIG_PATH="$(realpath "$_configdir")"
     _FROGUSR_DATA_PATH="$(realpath "$_datadir")"
 }
 
-_FROGUSR_PATH=""
+_FROGUSR_CONFIG_PATH=""
 _FROGUSR_DATA_PATH=""
