959b06cc81553da4c7fecf875f4ec73fb27aa4f9
roylaurie <roy.laurie@gmail.com>
2021-07-09 20:01:46 -0700

    licensing

--- asmov/bullfrog-common/bash/lib/bullfrog.lib.bash
+++ asmov/bullfrog-common/bash/lib/bullfrog.lib.bash
@@ -1,4 +1,22 @@
 #!/bin/bash
+##
+# Copyright (c) 2021 Asmov LLC
+# Licensed under GPLv3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###
+#
 # Bullfrog Bash Toolkit Library
 #
 # This library is intended for developers that wish to rapidly implement their own command-line toolkits and tools.
@@ -23,6 +41,7 @@
 #
 # Custom namespaces (with modules) may be installed and imported to enhance the functionality of a bullfrog toolkit.
 # Each namespace module may have one "default" operation which may be omitted from the command line, without any parameters.
+##
 
 # Configure bash options:
 #  - allexport: treat this script as a library and export all global variables and functions
