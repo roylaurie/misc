66e4f90a27be12574c7abe7799281ca2933fe879
roylaurie <roy.laurie@gmail.com>
2021-07-09 17:55:10 -0700

    cpu, ram, disk stats

--- asmov/bullfrog-common/bash/module/common/stats.module.bash
+++ asmov/bullfrog-common/bash/module/common/stats.module.bash
@@ -2,24 +2,40 @@
 set -o errexit -o pipefail -o privileged -o nounset 
 
 op_common_stats_default () {
+    local _result
+
     frogl_header "common.stats"
 
-    frogl_bullet "os-release"
+    frogl_bullet "/etc/os-release"
 
-    local _dist
-    _dist="$(cat /etc/os-release | head -2 | sed -re 's/^.*"(.+)".*$/\1/g' | tr '\n' ' ')"
+    _result="$(cat /etc/os-release | head -2 | sed -re 's/^.*"(.+)".*$/\1/g' | tr '\n' ' ')"
 
-    frogl_print_data "OS" "$_dist"
+    frogl_print_data "OS" "$_result"
 
     frogl_spacer
     frogl_bullet "iostat"
 
-    local _iostat
-    _iostat=($(echo "$(iostat)" | head -1))
+    _result=($(echo "$(iostat)" | head -1))
 
-    frogl_print_data "Kernel" "${_iostat[1]}"
-    frogl_print_data "Arch" "${_iostat[4]:1:-1}"
-    frogl_print_data "Hostname" "${_iostat[2]:1:-1}"
+    frogl_print_data "Kernel" "${_result[1]}"
+    frogl_print_data "Arch" "${_result[4]:1:-1}"
+    frogl_print_data "Hostname" "${_result[2]:1:-1}"
+
+    frogl_spacer
+    frogl_bullet "/proc"
+
+    _result="$(cat /proc/cpuinfo | grep '^model name' | head -1 | sed -e 's/^.*: //g')"
+    frogl_print_data "CPU" "$_result"
+
+    _result="$(cat /proc/meminfo | grep '^MemTotal' | sed -re 's/^.*:\s*//g' -e 's/ kB//g')"
+    _result="$(echo "$_result / 1000000" | bc) gb"
+    frogl_print_data "RAM" "$_result"
+
+    frogl_spacer
+    frogl_bullet "df"
+
+    mapfile -t _result <<< "$(df -h | grep "\/$" | awk '{ print $2 ; print $4 }')"
+    frogl_print_data "Root" "${_result[0]:0:-1} gb (total) / ${_result[1]:0:-1} gb (free)"
 
     frogl_footer
 }
